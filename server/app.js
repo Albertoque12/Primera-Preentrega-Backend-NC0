6b370311e9bd170afd89f5bfb70569fb386dbf5e
Alberto Orozco <kirarap@hotmail.com>
2023-03-08 19:22:32 -0800

    subiendo entregable cuatro

--- server/app.js
+++ server/app.js
@@ -1,12 +1,20 @@
 import express from 'express'
 import cRouter from '../routes/cartRoute.js'
 import pRouter from '../routes/productRoute.js'
+import handlebars from 'handlebars'
+
+
 const app = express()
 const PORT = 8080
 app.use(express.json());
 app.use(express.urlencoded( {extended: true} ));
 
 
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", main.handlebars + "/public/views");
+app.set("/public/views");
+app.set("view engine", "handlebars");
 
 
 app.use("/api/cart", cRouter)
